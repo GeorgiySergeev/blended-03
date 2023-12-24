448aff6cc717e3f79edec924a52c957ce9b6b5fa
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-12-24 11:19:02 +0200

    countries list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,18 @@
 import { Header } from 'components';
 import { CountrySearch, Home, Country } from 'pages';
+import { routes } from 'routes';
+
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 export const App = () => {
-  return <h2>App</h2>;
+  return (
+    <Routes>
+      <Route path={routes.HOME} element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path={routes.COUNTRY} element={<CountrySearch />} />
+        <Route path={routes.COUNTRY_ID} element={<Country />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react';
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fatchCountries = async () => {
+      setLoader(true);
+      setError(false);
+
+      try {
+        const data = await getCountries();
+        console.log(data);
+        setCountries(data);
+      } catch (error) {
+        setError(true);
+        console.log(error.message);
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    fatchCountries();
+  }, []);
+
   return (
     <Section>
+      {loader && <Loader />}
+
       <Container>
-        <h2>Home</h2>
+        {error && <Heading>Something go wrong!</Heading>}
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
