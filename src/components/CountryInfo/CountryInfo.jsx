aa39cb7173ee47cc875f40c0a4987ab30ab66126
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-12-24 12:43:47 +0200

    CountryInfo

--- src/components/CountryInfo/CountryInfo.jsx
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -8,36 +8,45 @@ import {
   CountryDetail,
   Accent,
 } from './CountryInfo.styled';
+import { GoBackBtn } from 'components'; 
+import { useLocation } from 'react-router-dom';
+import { useRef } from 'react';
 
-export const CountryInfo = ({
+export const CountryInfo = ({ info: {
   flag,
   capital,
-  country,
-  id,
+  country,  
   languages = [],
-  population,
+  population }
 }) => {
-  return (
-    <h2>CountryInfo</h2>
-    // <CountryWrapper >
-    //   <Flag>
-    //     <Image />
-    //   </Flag>
-    //   <CountryDescription>
-    //     <CountryCapital>
-    //       Capital: <Accent></Accent>
-    //     </CountryCapital>
+  const location = useLocation()
+  const path = useRef(location?.state?.from ?? '/')
+  // console.log(location)
+  return ( <> 
+    <GoBackBtn path={path.current}>
+        Go back
+      </GoBackBtn>
+        <CountryWrapper >
+      
+      <Flag>
+        <Image src={flag} alt={country } />
+      </Flag>
+      <CountryDescription>
+        <CountryCapital>
+          Capital: <Accent>{ capital}</Accent>
+        </CountryCapital>
 
-    //     <CountryTitle></CountryTitle>
+        <CountryTitle>{ country}</CountryTitle>
 
-    //     <CountryDetail>
-    //       Population: <Accent></Accent>
-    //     </CountryDetail>
+        <CountryDetail>
+          Population: <Accent>{ population}</Accent>
+        </CountryDetail>
 
-    //     <CountryDetail>
-    //       Languages: <Accent></Accent>
-    //     </CountryDetail>
-    //   </CountryDescription>
-    // </CountryWrapper>
-  );
+        <CountryDetail>
+          Languages: <Accent>{ languages.join(", ")}</Accent>
+        </CountryDetail>
+      </CountryDescription>
+    </CountryWrapper>
+    </>
+    );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,36 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {fetchCountry} from 'service/country-service'
 
 export const Country = () => {
+
+  const [country, setCountry] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const { id } = useParams()
+  
+  useEffect(() => {
+    const countryInfo = async () => {
+    setLoading(true)  
+      try {
+        const data = await fetchCountry(id)
+        setCountry(data)
+      } catch (error) {
+        console.log(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    countryInfo()
+  }, [id])
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        {loading && <Loader/>}
+        <CountryInfo info={country} />
       </Container>
     </Section>
   );
